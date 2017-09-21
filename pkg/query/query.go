6d78ec14809bc6476d0192dd68651324b1c6bce5
flo <flo@cis.lmu.de>
2017-09-21 15:27:44 +0200

    implement query lexer and tests

---
+++ pkg/query/query.go
@@ -0,0 +1,145 @@
+package query
+
+import (
+	"errors"
+	"fmt"
+	"unicode"
+)
+
+// Lexer identifies the lexems in a query.
+type Lexer struct {
+	str string
+	pos int
+}
+
+type lexerError string
+
+// LexemTypes
+const (
+	LexemIdent   = 0
+	LexemLeft    = '<'
+	LexemRight   = '>'
+	LexemQuest   = '?'
+	LexemOBrace  = '('
+	LexemCBrace  = ')'
+	LexemOBracet = '{'
+	LexemCBracet = '}'
+	LexemComma   = ','
+)
+
+// Lexeme represents a lexem.
+type Lexeme struct {
+	Str string
+	Typ int
+}
+
+// String returns the string representation of the lexem.
+func (l Lexeme) String() string {
+	return l.Str
+}
+
+// NewLexer create a new Lexer.
+func NewLexer(str string) *Lexer {
+	return &Lexer{str: str}
+}
+
+// Lex returns a slice of lexemes.
+func (l *Lexer) Lex() (ls []Lexeme, err error) {
+	defer func() {
+		if r, ok := recover().(lexerError); ok {
+			ls = nil
+			err = errors.New(string(r))
+		}
+	}()
+	for {
+		lexeme, done := l.nextLexem()
+		fmt.Printf("lexeme, done = %s, %t\n", lexeme, done)
+		if done {
+			return ls, nil
+		}
+		ls = append(ls, lexeme)
+	}
+}
+
+func (l *Lexer) peek() byte {
+	if l.pos >= len(l.str) {
+		return 0
+	}
+	return l.str[l.pos]
+}
+
+func (l *Lexer) next() byte {
+	if l.pos >= len(l.str) {
+		return 0
+	}
+	b := l.str[l.pos]
+	l.pos++
+	return b
+}
+
+func (l *Lexer) skip() {
+	// just recognices ascii whitespace
+	for isws(l.peek()) {
+		l.next()
+	}
+}
+
+func (l *Lexer) nextLexem() (Lexeme, bool) {
+	l.skip()
+	c := l.peek()
+	switch {
+	case isop(c):
+		l.next()
+		return Lexeme{Typ: int(c), Str: l.str[l.pos-1 : l.pos]}, false
+	case c == '\'' || c == '"':
+		l.next()
+		return l.parseQuotedIdent(c), false
+	case c == 0:
+		return Lexeme{}, true
+	default:
+		return l.parseIdent(), false
+	}
+}
+
+func (l *Lexer) parseQuotedIdent(q byte) Lexeme {
+	spos := l.pos
+	for {
+		c := l.next()
+		switch {
+		case c == q:
+			return Lexeme{Typ: LexemIdent, Str: l.str[spos : l.pos-1]}
+		case c == 0:
+			panic(lexerError("missing quotation"))
+		}
+	}
+	// panic(lexerError("not reached"))
+}
+
+func (l *Lexer) parseIdent() Lexeme {
+	spos := l.pos
+	for {
+		c := l.peek()
+		switch {
+		case isop(c) || c == '\'' || c == '"' || isws(c):
+			return Lexeme{Typ: LexemIdent, Str: l.str[spos:l.pos]}
+		case c == 0:
+			return Lexeme{Typ: LexemIdent, Str: l.str[spos:]}
+		default:
+			l.next()
+		}
+	}
+	// panic(lexerError("not reached"))
+}
+
+func isws(c byte) bool {
+	return unicode.IsSpace(rune(c))
+}
+
+func isop(c byte) bool {
+	switch {
+	case c == '?' || c == '<' || c == '>' || c == '(' || c == ')' || c == '{' || c == '}' || c == ',':
+		return true
+	default:
+		return false
+	}
+}
---
+++ pkg/query/query_test.go
@@ -0,0 +1,35 @@
+package query
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		query, want string
+		iserr       bool
+	}{
+		{"?(<R{A,B})", "[? ( < R { A , B } )]", false},
+		{"?(>R{'Qu ote',B})", "[? ( > R { Qu ote , B } )]", false},
+		{`?(>R{"()?{}<>",B})`, "[? ( > R { ()?{}<> , B } )]", false},
+		{`?(> R { "quot ed" , A } )`, "[? ( > R { quot ed , A } )]", false},
+		{`?(<{ident1 ident2`, "[? ( < { ident1 ident2]", false},
+		{`?(>R{"Missing quote, A})`, "[]", true},
+	}
+	for _, tc := range tests {
+		t.Run(tc.query, func(t *testing.T) {
+			lexer := NewLexer(tc.query)
+			ls, err := lexer.Lex()
+			if tc.iserr && err == nil {
+				t.Fatalf("expected error")
+			}
+			if !tc.iserr && err != nil {
+				t.Fatalf("got error: %v", err)
+			}
+			if str := fmt.Sprintf("%v", ls); str != tc.want {
+				t.Fatalf("expected %q; got %q", tc.want, str)
+			}
+		})
+	}
+}
