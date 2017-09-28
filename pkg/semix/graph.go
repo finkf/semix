63ee524dbbe99127207e9ec725cf55053e720f30
flo <flo@cis.lmu.de>
2017-09-28 18:34:02 +0200

    add documentation

--- pkg/semix/graph.go
+++ pkg/semix/graph.go
@@ -1,14 +1,20 @@
 package semix
 
+// Triple represents a relational triple in the graph.
+// It consitst of a subject S, a predicate P and an object O.
 type Triple struct {
 	S, P, O *Concept
 }
 
+// Graph represents a graph of linked concepts.
+// It holds a map of the URLs and the concepts and
+// an array of all concepts.
 type Graph struct {
 	cMap map[string]*Concept
 	cArr []*Concept
 }
 
+// NewGraph creates a new graph.
 func NewGraph() *Graph {
 	return &Graph{
 		cMap: make(map[string]*Concept),
@@ -16,6 +22,7 @@ func NewGraph() *Graph {
 	}
 }
 
+// FindByURL searches a concept by its URL.
 func (g *Graph) FindByURL(str string) (*Concept, bool) {
 	if c, ok := g.cMap[str]; ok {
 		return c, true
@@ -23,6 +30,7 @@ func (g *Graph) FindByURL(str string) (*Concept, bool) {
 	return nil, false
 }
 
+// FindById searches a concept by its ID.
 func (g *Graph) FindById(id int32) (*Concept, bool) {
 	if id == 0 {
 		return nil, false
@@ -36,6 +44,9 @@ func (g *Graph) FindById(id int32) (*Concept, bool) {
 	return g.cArr[id-1], true
 }
 
+// Add adds a triple to the graph.
+// It returns a Triple that consits of the according concepts
+// that where created.
 func (g *Graph) Add(s, p, o string) Triple {
 	if s == "" || p == "" || o == "" {
 		panic("cannot insert empty concept")
