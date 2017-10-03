2bac2495d2185560387bb74e26938efb2d76f40a
flo <flo@cis.lmu.de>
2017-10-03 19:55:41 +0200

    write wrapper class for sparsetable.FuzzyDfa

---
+++ pkg/semix/fuzzydfa.go
@@ -0,0 +1,43 @@
+package semix
+
+import (
+	"fmt"
+
+	"bitbucket.org/fflo/sparsetable"
+)
+
+// FuzzyDFA is a simple wrapper around a sparsetable.FuzzyDFA.
+// It maps the ids of the underlying sparsetable.DFA to Concepts.
+type FuzzyDFA struct {
+	dfa   *sparsetable.FuzzyDFA
+	graph *Graph
+}
+
+// NewFuzzyDFA constructs a new FuzzyDFA with the given maximum error bound k.
+func NewFuzzyDFA(k int, dfa DFA) FuzzyDFA {
+	return FuzzyDFA{
+		dfa:   sparsetable.NewFuzzyDFA(k, dfa.dfa),
+		graph: dfa.graph,
+	}
+}
+
+// MaxError returns the maximum allowed error for the this FuzzyDFA.
+func (d FuzzyDFA) MaxError() int {
+	return d.dfa.MaxError()
+}
+
+// Initial returns the initial state of this FuzzyDFA.
+func (d FuzzyDFA) Initial(str string) sparsetable.FuzzyStack {
+	return d.dfa.Initial(str)
+}
+
+// Delta executes one fuzzy transition in this FuzzyDFA.
+func (d FuzzyDFA) Delta(s sparsetable.FuzzyStack, f func(int, int, *Concept)) sparsetable.FuzzyStack {
+	return d.dfa.Delta(s, func(k, pos int, id int32) {
+		c, ok := d.graph.FindByID(id)
+		if !ok {
+			panic(fmt.Sprintf("invalid id: %d", id))
+		}
+		f(k, pos, c)
+	})
+}
