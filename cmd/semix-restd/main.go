2fce1f76a329774e0bc9a2f13381781eaf7aa378
flo <flo@cis.lmu.de>
2017-11-06 12:11:23 +0100

    improve configuration of  rest daemon

--- cmd/semix-restd/main.go
+++ cmd/semix-restd/main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"bitbucket.org/fflo/semix/pkg/index"
 	"bitbucket.org/fflo/semix/pkg/rdfxml"
 	"bitbucket.org/fflo/semix/pkg/rest"
+	"bitbucket.org/fflo/semix/pkg/semix"
 	"bitbucket.org/fflo/semix/pkg/traits"
+	"bitbucket.org/fflo/semix/pkg/turtle"
+	"github.com/BurntSushi/toml"
 )
 
 var (
-	dir  string
-	host string
-	rdf  string
-	help bool
+	dir   string
+	host  string
+	confg string
+	help  bool
 )
 
 func init() {
@@ -24,10 +30,7 @@ func init() {
 		filepath.Join(os.Getenv("HOME"), "semix"),
 		"set semix index directory")
 	flag.StringVar(&host, "host", "localhost:6060", "set listen host")
-	flag.StringVar(&rdf, "rdf",
-		filepath.Join(os.Getenv("HOME"),
-			"/devel/priv/semix/misc/data/topiczoom.skos.rdf.xml"),
-		"set RDF input file")
+	flag.StringVar(&confg, "config", "testdata/topiczoom.toml", "set configuration file")
 	flag.BoolVar(&help, "help", false, "prints this help")
 }
 
@@ -37,38 +40,71 @@ func main() {
 		flag.Usage()
 		return
 	}
-	index, err := index.New(
-		dir,
-		index.WithBufferSize(5),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	is, err := os.Open(rdf)
-	if err != nil {
-		log.Fatal(err)
-	}
+	index, err := index.New(dir)
+	panicIf(err)
+	config, err := readConfig(confg)
+	panicIf(err)
+	log.Printf("config: %v", config)
+	is, err := os.Open(config.Parser.File)
+	panicIf(err)
 	defer is.Close()
-	t := traits.New(
-		traits.WithIgnoreURLs(
-			"http://www.w3.org/2004/02/skos/core#narrower",
-		),
-		traits.WithTransitiveURLs(
-			"http://www.w3.org/2004/02/skos/core#broader",
-			"http://www.w3.org/2004/02/skos/core#narrower",
-		),
-		traits.WithNameURLs(
-			"http://www.w3.org/2004/02/skos/core#prefLabel",
-		),
-		traits.WithDistinctURLs(
-			"http://www.w3.org/2004/02/skos/core#altLabel",
-		),
-	)
-	parser := rdfxml.NewParser(is)
-	s, err := rest.New(host, parser, t, index)
+	parser, err := newParser(is, config)
+	panicIf(err)
+	s, err := rest.New(host, parser, config.traits(), index)
+	panicIf(err)
+	log.Printf("starting the server")
+	log.Fatal(s.ListenAndServe())
+}
+
+func panicIf(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("starting server")
-	log.Fatal(s.ListenAndServe())
+}
+
+type parser struct {
+	File, Type string
+}
+
+type urls struct {
+	Ignore     []string
+	Transitive []string
+	Symmetric  []string
+	Name       []string
+	Distinct   []string
+	Ambiguous  []string
+}
+
+type config struct {
+	Parser parser
+	URLs   urls
+}
+
+func (c config) traits() semix.Traits {
+	return traits.New(
+		traits.WithIgnoreURLs(c.URLs.Ignore...),
+		traits.WithTransitiveURLs(c.URLs.Transitive...),
+		traits.WithNameURLs(c.URLs.Name...),
+		traits.WithAmbiguousURLs(c.URLs.Ambiguous...),
+		traits.WithDistinctURLs(c.URLs.Distinct...),
+	)
+}
+
+func readConfig(file string) (config, error) {
+	var c config
+	if _, err := toml.DecodeFile(file, &c); err != nil {
+		return config{}, err
+	}
+	return c, nil
+}
+
+func newParser(r io.Reader, c config) (semix.Parser, error) {
+	switch strings.ToLower(c.Parser.Type) {
+	case "rdfxml":
+		return rdfxml.NewParser(r), nil
+	case "turtle":
+		return turtle.NewParser(r), nil
+	default:
+		return nil, fmt.Errorf("invalid type: %s", c.Parser.Type)
+	}
 }
