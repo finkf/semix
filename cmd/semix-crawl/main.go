480c20991c0070b0cf7adee6899e229ce66158da
flo <flo@cis.lmu.de>
2017-11-14 20:36:58 +0100

    implement pooling (not working)

--- cmd/semix-crawl/main.go
+++ cmd/semix-crawl/main.go
@@ -7,12 +7,14 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	"bitbucket.org/fflo/semix/pkg/config"
 	"bitbucket.org/fflo/semix/pkg/index"
@@ -21,21 +23,26 @@ import (
 )
 
 var (
-	dir        string
-	configFile string
-	prefix     *regexp.Regexp
-	help       bool
-	max        int
+	dir       string
+	conf      string
+	allowed   *regexp.Regexp
+	forbidden *regexp.Regexp
+	help      bool
+	max       int
+	jobs      int
 )
 
 func init() {
-	var pre string
-	flag.StringVar(&pre, "prefix", "^https?://de.wikipedia.org/wiki", "allowed url prefix regexp")
-	prefix = regexp.MustCompile(pre)
+	var a, f string
+	flag.StringVar(&a, "allow", "^https?://de.wikipedia.org/wiki", "allowed url regexp")
+	flag.StringVar(&f, "forbid", "Datei:", "forbidden url regexp")
 	flag.StringVar(&dir, "dir", filepath.Join(os.Getenv("HOME"), "semix"), "semix index directory")
-	flag.StringVar(&configFile, "config", "semix.toml", "configuration file")
+	flag.StringVar(&conf, "config", "semix.toml", "configuration file")
 	flag.BoolVar(&help, "help", false, "print help")
-	flag.IntVar(&max, "max", 1000, "max number of documents to process")
+	flag.IntVar(&max, "max", 10, "max number of documents to process")
+	flag.IntVar(&jobs, "jobs", 100, "number of jobs")
+	forbidden = regexp.MustCompile(f)
+	allowed = regexp.MustCompile(a)
 }
 
 func main() {
@@ -50,10 +57,11 @@ func main() {
 var (
 	dispatchc chan *url.URL
 	indexerc  chan webpage
+	pool      chan *url.URL
 )
 
 func run(args []string) {
-	g, d, err := config.Parse(configFile)
+	g, d, err := config.Parse(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -61,14 +69,15 @@ func run(args []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer idx.Close()
 	dispatchc = make(chan *url.URL, len(args))
 	indexerc = make(chan webpage)
+	pool = make(chan *url.URL, jobs)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	stream := index.Put(ctx, idx, semix.Filter(ctx,
 		semix.Match(ctx, semix.DFAMatcher{DFA: semix.NewDFA(d, g)},
 			semix.Normalize(ctx, crawl(ctx)))))
-	fmt.Printf("Args: %v\n", args)
 	for _, arg := range args {
 		url, err := url.Parse(arg)
 		if err != nil {
@@ -77,9 +86,15 @@ func run(args []string) {
 		}
 		dispatchc <- url
 	}
+	var tokens int
 	for t := range stream {
-		log.Printf("%s", t.Token)
+		if t.Err != nil {
+			log.Printf("error: %s", t.Err)
+		} else {
+			tokens++
+		}
 	}
+	log.Printf("indexed %d tokens", tokens)
 }
 
 type webpage struct {
@@ -94,6 +109,7 @@ func crawl(ctx context.Context) semix.Stream {
 		defer cancel()
 		defer close(cstream)
 		go dispatcher(cctx)
+		go pooler(cctx)
 		var n int
 		for n < max {
 			select {
@@ -123,27 +139,54 @@ func crawl(ctx context.Context) semix.Stream {
 
 func dispatcher(ctx context.Context) {
 	urlset := make(map[string]bool)
+loop:
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case url := <-dispatchc:
-			if urlset[url.String()] {
-				// log.Printf("skipping %s", url)
-				break
+			if !shouldHandleURL(urlset, url) {
+				continue loop
 			}
-			urlset[url.String()] = true
-			if prefix.FindString(url.String()) == "" {
-				// log.Printf("skipping %s", url)
-				break
+			select {
+			case <-ctx.Done():
+				return
+			case pool <- url:
 			}
+		}
+	}
+}
+
+func shouldHandleURL(urlset map[string]bool, url *url.URL) bool {
+	str := url.String()
+	if urlset[str] {
+		return false
+	}
+	urlset[str] = true
+	if allowed.FindString(str) == "" {
+		return false
+	}
+	if forbidden.FindString(str) != "" {
+		return false
+	}
+	return true
+}
+
+func pooler(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case url := <-pool:
 			go handle(ctx, url)
 		}
 	}
 }
 
 func handle(ctx context.Context, url *url.URL) {
-	// log.Printf("sending request: [GET] %s", url)
+	ms := time.Duration(rand.Intn(10000))
+	time.Sleep(time.Duration(ms * time.Millisecond))
+	log.Printf("sending request: [GET] %s", url)
 	res, err := http.Get(url.String())
 	if err != nil {
 		log.Printf("error: [GET] %s: %s", url, err)
@@ -159,16 +202,17 @@ func handle(ctx context.Context, url *url.URL) {
 		log.Printf("could not copy content of %s: %s", url, err)
 		return
 	}
-	go dispatchLinks(ctx, bytes.NewBuffer(buffer.Bytes()), url)
+	content := buffer.String()
+	go dispatchLinks(ctx, content, url)
 	select {
 	case <-ctx.Done():
-	case indexerc <- webpage{url, buffer.String()}:
+	case indexerc <- webpage{url, content}:
 	}
 }
 
-func dispatchLinks(ctx context.Context, r io.Reader, base *url.URL) {
+func dispatchLinks(ctx context.Context, content string, base *url.URL) {
 	go func() {
-		doc, err := html.Parse(r)
+		doc, err := html.Parse(strings.NewReader(content))
 		if err != nil {
 			log.Printf("error parsing html: %s", err)
 		}
