ae783eeb9add9cdcc177156addf3860194f46961
flo <flo@cis.lmu.de>
2017-11-15 10:49:52 +0100

    encode direct/indirect information within L field of dse

--- pkg/index/dse.go
+++ pkg/index/dse.go
@@ -19,13 +19,13 @@ func newDSE(e Entry, register func(string) int) dse {
 		P: e.Path,
 		B: uint32(e.Begin),
 		E: uint32(e.End),
-		L: encodeL(e.L, e.Ambiguous),
+		L: encodeL(e.L, e.Ambiguous, e.RelationURL != ""),
 		R: int32(register(e.RelationURL)),
 	}
 }
 
 func (d dse) entry(conceptURL string, lookup func(int) string) Entry {
-	l, a := decodeL(d.L)
+	l, a, _ := decodeL(d.L)
 	return Entry{
 		ConceptURL:  conceptURL,
 		RelationURL: lookup(int(d.R)),
@@ -38,14 +38,21 @@ func (d dse) entry(conceptURL string, lookup func(int) string) Entry {
 	}
 }
 
-func encodeL(l int, a bool) uint8 {
-	x := uint8(l) & 0x7f
+// 0x3f -> 0011.1111
+// 0x80 -> 1000.0000
+// 0x40 -> 0100.0000
+// max encodeable levenshtein distance -> 0x3f -> 63
+func encodeL(l int, a, d bool) uint8 {
+	x := uint8(l) & 0x3f
 	if a {
 		x |= 0x80
+	}
+	if d {
+		x |= 0x40
 	}
 	return x
 }
 
-func decodeL(x uint8) (int, bool) {
-	return int(x & 0x7f), x&0x80 > 0
+func decodeL(x uint8) (int, bool, bool) {
+	return int(x & 0x3f), x&0x80 > 0, x&0x40 > 0
 }
