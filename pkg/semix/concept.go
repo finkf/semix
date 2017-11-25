a824695d496f0f3e4cc33f83322e9a41a0207cbc
flo <flo@cis.lmu.de>
2017-11-25 17:20:15 +0100

    fix test regression

--- pkg/semix/concept.go
+++ pkg/semix/concept.go
@@ -21,7 +21,7 @@ type Edge struct {
 }
 
 func (e Edge) String() string {
-	return fmt.Sprintf("{%q %q %d}", conceptName(e.P), conceptName(e.O), e.L)
+	return fmt.Sprintf("{%s %s %d}", conceptName(e.P), conceptName(e.O), e.L)
 }
 
 // Concept represents a concept in the concept graph.
@@ -148,7 +148,7 @@ func (c *Concept) ShortURL() string {
 }
 
 func (c *Concept) String() string {
-	return fmt.Sprintf("{%q %d %v}", conceptName(c), c.id, c.edges)
+	return fmt.Sprintf("{%s %d %v}", conceptName(c), c.id, c.edges)
 }
 
 func conceptName(c *Concept) string {
