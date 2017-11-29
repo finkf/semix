4e41e7bd1b3935b7fa3464c372b61ca4b61c670b
flo <flo@cis.lmu.de>
2017-11-29 19:41:40 +0100

    use args for article regexes

--- cmd/semix-wiki/main.go
+++ cmd/semix-wiki/main.go
@@ -3,47 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
-	"regexp"
-	"strings"
+
+	"bitbucket.org/fflo/semix/pkg/args"
 )
 
 var (
-	ms matches
+	articles args.RegexList
 )
 
 func init() {
-	flag.Var(&ms, "m", "list of regexes to match articles")
+	flag.Var(&articles, "a", "list of regexes to match articles")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Printf("matches: %s\n", ms)
+	fmt.Printf("articles: %s\n", articles)
 }
 
-type matches []*regexp.Regexp
-
-func (m matches) String() string {
-	var strs []string
-	for _, re := range m {
-		strs = append(strs, re.String())
-	}
-	return strings.Join(strs, ",")
-}
-
-func (m *matches) Set(val string) error {
-	re, err := regexp.Compile(val)
-	if err != nil {
-		return err
-	}
-	*m = append(*m, re)
-	return nil
-}
-
-func (m matches) Match(article string) bool {
-	if len(m) == 0 {
+func match(article string) bool {
+	if len(articles) == 0 {
 		return true
 	}
-	for _, re := range m {
+	for _, re := range articles {
 		if re.FindString(article) != "" {
 			return true
 		}
