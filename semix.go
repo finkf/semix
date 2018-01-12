feffb3c053a3b08cab181a77fce0ac2d242bf4c7
flo <flo@cis.lmu.de>
2018-01-12 11:08:49 +0100

    cleanup

--- semix.go
+++ semix.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		// no need to print error
+		// no need to print error message
 		// since cobra takes care of this
-		// fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
 }
